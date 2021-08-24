5c335c2d23568829347aff05af9e7c62301879b7
WooJongSeong <whdtud@gmail.com>
2021-08-24 20:40:13 +0900

    커밋 제외 폴더 추가

--- BoardWeb/src/main/java/com/springbook/view/controller/DispatcherServlet.java
+++ BoardWeb/src/main/java/com/springbook/view/controller/DispatcherServlet.java
@@ -22,6 +22,24 @@ public class DispatcherServlet extends HttpServlet {
 	}
 
 	private void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String uri = request.getRequestURI();
+		String path = uri.substring(uri.lastIndexOf("/"));
+		System.out.println(path);
 		
+		if (path.equals("/login.do")) {
+			System.out.println("로그인 처리");
+		} else if (path.equals("/logout.do")) {
+			System.out.println("로그아웃 처리");
+		} else if (path.equals("/insertBoard.do")) {
+			System.out.println("글 등록 처리");
+		} else if (path.equals("/updateBoard.do")) {
+			System.out.println("글 수정 처리");
+		} else if (path.equals("/deleteBoard.do")) {
+			System.out.println("글 삭제 처리");
+		} else if (path.equals("/getBoard.do")) {
+			System.out.println("글 상세 조회 처리");
+		} else if (path.equals("/getBoardList.do")) {
+			System.out.println("글 목록 검색 처리");
+		}
 	}
 }
